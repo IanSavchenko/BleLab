05e09660aef7bdea7e72f62ad72bc0cb8782b8c9
Ian Savchenko <IanSavchenko@users.noreply.github.com>
2017-10-22 23:02:56 +0200

    An attempt to create tests for keeper, but apparently Mocks do not work with R# test runner

---
+++ BleLab.Tests/Settings/KeeperTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using BleLab.Settings;
+using HyperMock;
+using Xunit;
+
+namespace BleLab.Tests.Settings
+{
+    public class KeeperTests
+    {
+        private TestKeeper _instance;
+        private Mock<IKeeperStorage> _storageMock = Mock.Create<IKeeperStorage>();
+
+        public KeeperTests()
+        {
+            _instance = new TestKeeper(_storageMock.Object);
+        }
+
+        [Fact]
+        public void ReadingSimpleProperty_IfNotDefined_ShouldReturnDefault()
+        {
+            Assert.True(_instance.JustProperty == default(bool));
+
+            _storageMock.Verify(s => s.HasKey(nameof(TestKeeper.JustProperty)), Occurred.Once());
+            _storageMock.Verify(s => s.GetValue(nameof(TestKeeper.JustProperty)), Occurred.Never());
+        }
+
+    }
+
+    public class TestKeeper : Keeper
+    {
+        public TestKeeper(IKeeperStorage storage) : base(storage)
+        {
+        }
+
+        public bool JustProperty
+        {
+            get => Get<bool>();
+            set => Set(value);
+        }
+
+        [KeeperProperty]
+        public bool PropertyWithAttribute
+        {
+            get => Get<bool>();
+            set => Set(value);
+        }
+
+        [KeeperProperty(PersistentKey = "MyKey")]
+        public bool PropertyWithPersistentKey
+        {
+            get => Get<bool>();
+            set => Set(value);
+        }
+
+        [KeeperProperty(DefaultValue = true)]
+        public bool PropertyWithDefaultValue
+        {
+            get => Get<bool>();
+            set => Set(value);
+        }
+    }
+}
